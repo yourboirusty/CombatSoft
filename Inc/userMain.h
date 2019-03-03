1661c0e0426c65c8a0aa2d27e3b7e382c29fdd24
Kacper Chmielewski <kacper.chmielewski@blebox.eu>
2019-03-03 14:55:09 +0100

    userMain init

---
+++ Inc/userMain.h
@@ -0,0 +1,13 @@
+/*
+ * userMain.h
+ *
+ *  Created on: Mar 3, 2019
+ *      Author: kacper
+ */
+
+#ifndef USERMAIN_H_
+#define USERMAIN_H_
+
+void userMain();
+
+#endif /* USERMAIN_H_ */
---
+++ Src/userMain.c
@@ -0,0 +1,12 @@
+/*
+ * userMain.c
+ *
+ *  Created on: Mar 3, 2019
+ *      Author: kacper
+ */
+
+#include "userMain.h"
+
+void userMain() {
+
+}

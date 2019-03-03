c0c35bae65ae0e19e26fc5a1b33ff0c321a92fd6
Kacper Chmielewski <kacper.chmielewski@blebox.eu>
2019-03-03 14:59:25 +0100

    SSL files init

---
+++ Inc/SanwaSynchronaizedLink.h
@@ -0,0 +1,13 @@
+/*
+ * SanwaSynchronaizedLink.h
+ *
+ *  Created on: Mar 3, 2019
+ *      Author: kacper
+ */
+
+#ifndef SANWASYNCHRONAIZEDLINK_H_
+#define SANWASYNCHRONAIZEDLINK_H_
+
+
+
+#endif /* SANWASYNCHRONAIZEDLINK_H_ */
--- Src/SanwaSynchronaizedLink.c
+++ Src/SanwaSynchronaizedLink.c
@@ -1,14 +1,8 @@
 /*
- * userMain.c
+ * SanwaSynchronaizedLink.c
  *
  *  Created on: Mar 3, 2019
  *      Author: kacper
  */
 
-#include "userMain.h"
-
 #include "SanwaSynchronaizedLink.h"
-
-void userMain() {
-
-}
